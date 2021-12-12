8c3babf8d234d9ba295c20d8e048ac8e923490ec
sabah hussen <sabahhussen787@gmail.com>
2021-12-13 00:34:43 +0200

    clockwiseand anticlockwisefullcode

--- mycode.asm
+++ mycode.asm
@@ -12,28 +12,40 @@
      ;rotate motore in clockwise 
      
      mov AL , 80H ; initial value 10000000 
-     out CWR , AL 
+     out CWR , AL   
      mov AL , 88H ; 10001000 to start
-     mov CX,1000; counter
+     mov CX,400; counter
      
-   loop1: out PORTA,AL 
-          ROL AL,01 ;   AL=00010001 rotate left 
+   loop1: out PORTA,AL
+          call DELAY 
+          ROL AL,01 ;   AL=00010001  
           DEC CX    ; decreace counter
           
-       jnz loop1  
+       jnz loop1 
        
-       ; rotate motore in unti clockwise 
        
        MOV AH, 88H    ; initial value 10001000 
        mov cx,1000     ; counter
-   loop2: out PORTA,AL       
+   loop2: out PORTA,AL
+          call DELAY
           ROR AL,01       ;AL= 01000100 rotate right
           DEC CX
-          jnz loop2 
-           
+          jnz loop2    
+       
+      
+     ;-------------------   
+     ;DELAY FUNCTION  
+     DELAY PROC                               
+     MOV BX, CX
+     MOV CX, 06ffH
+      LOOP3:loop LOOP3
+     MOV CX, BX
+     RET
+     DELAY ENDP  
+     
+    ;--------------------     
        
        
-
        
        
       .exit 
